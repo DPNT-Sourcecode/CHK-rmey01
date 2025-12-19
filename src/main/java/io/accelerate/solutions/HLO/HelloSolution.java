219bda190e6177d6744c753512828a4bd1b98ac2
sbx_user1051 <sbx_user1051@169.254.37.61>
2025-12-19 03:55:59 +0000

    Fri Dec 19 03:55:59 UTC 2025

--- src/main/java/io/accelerate/solutions/HLO/HelloSolution.java
+++ src/main/java/io/accelerate/solutions/HLO/HelloSolution.java
@@ -2,8 +2,9 @@ package io.accelerate.solutions.HLO;
 
 public class HelloSolution {
     public String hello(String friendName) {
-        return "Hello, World!";
+        return "Hello, " + friendName + "!";
     }
 }
+
 
 
